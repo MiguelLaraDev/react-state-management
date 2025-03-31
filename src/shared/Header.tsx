c9ec2f71b09282f0e0a8c6e742942a3a272908ea
miguel-lara-selectra <miguel.lara@selectra.info>
2025-03-31 18:03:11 +0200

    Installed font awesome

--- src/shared/Header.tsx
+++ src/shared/Header.tsx
@@ -1,14 +1,24 @@
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import classNames from "classnames";
 
 const Header = () => {
   return (
     <header
       className={classNames(
-        "sticky w-full h-12 p-2 bg-neutral-50 shadow",
-        "flex flex-row items-center"
+        "sticky w-full h-12 py-2 px-6 bg-neutral-50 shadow",
+        "flex flex-row items-center justify-between"
       )}
     >
-      header
+      <div>left</div>
+
+      <div>logo</div>
+
+      <div>
+        <button>
+          <FontAwesomeIcon icon={faCartShopping} />
+        </button>
+      </div>
     </header>
   );
 };
