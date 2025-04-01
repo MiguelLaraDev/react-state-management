0782cc3a7b13839aa332cbbb3d7542f3d418ef6e
miguel-lara-selectra <miguel.lara@selectra.info>
2025-04-01 15:21:20 +0200

    Mounting infinit list logic

--- src/pages/InstrumentsList.tsx
+++ src/pages/InstrumentsList.tsx
@@ -1,7 +1,45 @@
 import { faCartPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
+
+const fetchItems = async ({ pageParam = 0 }) => {
+  const response = await fetch(`https://api.example.com/items?page=${pageParam}`);
+
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+
+  return response.json();
+};
 
 const InstrumentsList = () => {
+  const { data, error, status, fetchNextPage, isFetchingNextPage } = useInfiniteQuery({
+    queryKey: ["items"],
+    queryFn: fetchItems,
+    initialPageParam: 0,
+    getNextPageParam: (lastPage) => lastPage.nextPage,
+  });
+
+  const { ref, inView } = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      fetchNextPage();
+    }
+  }, [fetchNextPage, inView]);
+
+  if (status === "pending") {
+    return <div>Loading...</div>;
+  }
+
+  if (status === "error") {
+    return <div>Error: {error.message}</div>;
+  }
+
+  console.log(data);
+
   return (
     <div className='w-full flex flex-col gap-4'>
       {Array.from({ length: 100 }, (_, i) => (
@@ -33,6 +71,7 @@ const InstrumentsList = () => {
           </div>
         </div>
       ))}
+      <div ref={ref}>{isFetchingNextPage && "Loading..."}</div>
     </div>
   );
 };
