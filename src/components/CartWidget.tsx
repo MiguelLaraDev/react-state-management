15186a55bfafa2e9636434edb7020fd55f1bb26e
miguel-lara-selectra <miguel.lara@selectra.info>
2025-04-07 13:45:11 +0200

    wip Shoping cart

--- src/components/CartWidget.tsx
+++ src/components/CartWidget.tsx
@@ -1,17 +1,46 @@
 import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import classNames from "classnames";
+
+import { useState } from "react";
+import { createPortal } from "react-dom";
 import { useCartStore } from "../stores/cart.store";
 
 const CartWidget = () => {
-  const count = useCartStore((state) => state.cart.length);
+  const { cart } = useCartStore();
+  const count = cart.length;
+
+  const [expanded, setExpanded] = useState(false);
 
   return (
-    <div className='relative'>
-      {count > 0 && <span className='absolute'>{count}</span>}
-      <button>
-        <FontAwesomeIcon icon={faCartShopping} />
-      </button>
-    </div>
+    <>
+      {expanded &&
+        createPortal(
+          <div className='absolute top-12 right-6 z-50'>Shopping cart!</div>,
+          document.body
+        )}
+
+      <div className='relative'>
+        {count > 0 && (
+          <span
+            className={classNames(
+              "absolute -top-2 -left-3 w-4 h-4 bg-green-600 rounded-full",
+              "flex items-center justify-center",
+              "text-white text-xs"
+            )}
+          >
+            {count}
+          </span>
+        )}
+
+        <button
+          className='w-6 h-full hover:[&_svg]:text-neutral-400 cursor-pointer'
+          onClick={() => setExpanded((v) => !v)}
+        >
+          <FontAwesomeIcon icon={faCartShopping} />
+        </button>
+      </div>
+    </>
   );
 };
 export default CartWidget;
