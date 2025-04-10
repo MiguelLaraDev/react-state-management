88bdcd05b9f4650a8f6b87df32bfc927cb025626
miguel-lara-selectra <miguel.lara@selectra.info>
2025-04-10 07:39:22 +0200

    Desktop styles

--- src/pages/InstrumentDetail.tsx
+++ src/pages/InstrumentDetail.tsx
@@ -1,34 +1,54 @@
+import { Link } from "react-router-dom";
+
+import classNames from "classnames";
 import ImageGallery from "../components/ImageGallery";
 import Score from "../components/Score";
 import Button from "../components/ui-toolkit/Button";
 
 const InstrumentDetail = () => {
   return (
-    <div className='border border-red-500'>
-      <div>
+    <div className='w-full flex flex-col gap-4 border border-red-500'>
+      <div className='flex flex-row items-center gap-1'>
         <Score score={4} />
-        <p>811</p>
+        <p className='text-xs text-neutral-500 font-semibold pt-1'>811</p>
       </div>
 
-      <img src='/images/brands/logo' alt='??' />
+      <Link to='/brand/????' className='w-fit min-w-24 h-12 bg-neutral-100'>
+        <img src='/images/brands/logo' alt='???' />
+      </Link>
 
       <ImageGallery />
 
-      <p>B-stock from 66 € available</p>
+      {/* <p>B-stock from 66 € available</p> */}
 
-      <div>
-        <p>89 €</p>
-        <p>All prices incl. VAT</p>
+      <div className='flex flex-col gap-0'>
+        <p className='flex flex-row justify-start gap-1 font-bold text-5xl text-neutral-900'>
+          89 <span className='!text-lg pt-1'>€</span>
+        </p>
+        <p className='text-sm'>All prices incl. VAT</p>
       </div>
 
-      <p>In stock</p>
+      <p
+        className={classNames("text-sm", {
+          "text-green-700": true,
+        })}
+      >
+        In stock
+      </p>
 
-      <div>
+      <div className='flex flex-row items-center justify-between border'>
         <input type='number' value={1} />
+
         <Button label='ADD TO BASKET' onClick={() => console.log("add to cart")} />
       </div>
 
-      <p>Long description here</p>
+      {/* like, compare, sharing */}
+
+      <p className='text-base leading-snug tracking-normal text-neutral-700'>
+        Long description here Long description here Long description here Long description here Long
+        description here Long description here Long description here Long description here Long
+        description here Long description here Long description here Long description here{" "}
+      </p>
     </div>
   );
 };
